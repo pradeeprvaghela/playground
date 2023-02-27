834c7304a8d202ee75d0025b97ad0380e8896f77
pradeeprvaghela <121917439+pradeeprvaghela@users.noreply.github.com>
2023-02-27 01:13:34 -0500

    Project  4 (#1)
    * initial setup for project  4
    
    * Add bootstrap to project
    
    * Add Navbar to project 3
    
    * Add designes to project 3 and 4
    
    * do program using imperative way
    
    * Add another div in project4
    
    * change name 41 to 4_1
    
    * Fix spacing in index.html from project4

---
+++ project4/index.js
@@ -0,0 +1,10 @@
+const project4 = document.createElement("project4")
+project4.textContent = "learnig to do program using imperative way"
+project4.className = "header"
+document.getElementById("p4").append(project4)
+
+const project4_1 = document.createElement("project4_1")
+project4_1.textContent = "its new for me"
+project4_1.className = "page"
+document.getElementById("a1").append(project4_1)
+
