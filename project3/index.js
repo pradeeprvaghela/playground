1fead0600e7bfd01cd989bc90f06b9a11271aa13
pradeeprvaghela <121917439+pradeeprvaghela@users.noreply.github.com>
2023-02-26 22:44:11 -0500

    Add designes to project 3 and 4

--- project3/index.js
+++ project3/index.js
@@ -21,7 +21,7 @@ const Navbar = () => (
                             <ul className="dropdown-menu">
                                 <li><a className="dropdown-item" href="#">Action</a></li>
                                 <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider"/></li>
+                                <li><hr className="dropdown-divider" /></li>
                                 <li><a className="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                         </li>
@@ -30,8 +30,8 @@ const Navbar = () => (
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
                     </form>
                 </div>
             </div>
@@ -40,7 +40,102 @@ const Navbar = () => (
 )
 
 const MainContent = () => (
-    <h1>My name is Pradeep I am learning react</h1>
+    <div>
+        <div id="carouselExample" className="carousel slide">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="https://images4.alphacoders.com/973/thumb-1920-973967.jpg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://wallpaperaccess.com/full/21599.jpg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://d9jhi50qo719s.cloudfront.net/5il/samples/1osk_800.png?220916063611" className="d-block w-100" alt="..." />
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+
+
+        <div className="container marketing mt-5">
+
+            <div className="row">
+                <div className="col-lg-4">
+                    <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                    <h2 className="fw-normal">Heading</h2>
+                    <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                    <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+                </div>
+                <div className="col-lg-4">
+                    <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                    <h2 className="fw-normal">Heading</h2>
+                    <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+                    <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+                </div>
+                <div className="col-lg-4">
+                    <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                    <h2 className="fw-normal">Heading</h2>
+                    <p>And lastly this, the third column of representative placeholder content.</p>
+                    <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+                </div>
+            </div>
+
+
+
+
+            <hr className="featurette-divider" />
+
+            <div className="row featurette">
+                <div className="col-md-7">
+                    <h2 className="featurette-heading fw-normal lh-1">First featurette heading. <span className="text-muted">It’ll blow your mind.</span></h2>
+                    <p className="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+                </div>
+                <div className="col-md-5">
+                <img src="https://www.nicepng.com/png/detail/319-3195597_tumblr-orhkdms18s1tdj66po1-500-anime-girl-best-friends.png" className="img-fluid" />
+                </div>
+            </div>
+
+            <hr className="featurette-divider" />
+
+            <div className="row featurette">
+                <div className="col-md-7 order-md-2">
+                    <h2 className="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span className="text-muted">See for yourself.</span></h2>
+                    <p className="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+                </div>
+                <div className="col-md-5 order-md-1">
+                    <img src="https://i.pinimg.com/originals/e5/b7/61/e5b761e1df74d54911cee584ec1b8122.jpg" className="img-fluid" />
+                </div>
+            </div>
+
+            <hr className="featurette-divider" />
+
+            <div className="row featurette">
+                <div className="col-md-7">
+                    <h2 className="featurette-heading fw-normal lh-1">And lastly, this one. <span className="text-muted">Checkmate.</span></h2>
+                    <p className="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
+                </div>
+                <div className="col-md-5">
+                    <img src="https://images6.fanpop.com/image/photos/34500000/Naruto-naruto-34528162-500-409.jpg" className="img-fluid" />
+                </div>
+            </div>
+
+            <hr className="featurette-divider" />
+
+        </div>
+
+        <footer className="container">
+            <p className="float-end"><a href="#">Back to top</a></p>
+            <p>&copy; 2017–2022 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        </footer>
+    </div>
+
 )
 
 ReactDOM.render((
