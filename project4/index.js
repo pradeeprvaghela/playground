e4052f46d62d1fc86f82bd92d6b1b34cff3949cd
pradeeprvaghela <121917439+pradeeprvaghela@users.noreply.github.com>
2023-02-27 01:05:14 -0500

    Add another div in project4

--- project4/index.js
+++ project4/index.js
@@ -2,3 +2,9 @@ const project4 = document.createElement("project4")
 project4.textContent = "learnig to do program using imperative way"
 project4.className = "header"
 document.getElementById("p4").append(project4)
+
+const project41 = document.createElement("project41")
+project41.textContent = "its new for me"
+project41.className = "page"
+document.getElementById("a1").append(project41)
+
