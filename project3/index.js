fcd0821fdda081604f3f00c24522188260ce565f
pradeeprvaghela <121917439+pradeeprvaghela@users.noreply.github.com>
2023-02-26 20:58:29 -0500

    made chnages in function

--- project3/index.js
+++ project3/index.js
@@ -1,12 +1,12 @@
-function MainContent() {
-    return(
-        <h1>My name is Pradeep I am learning react</h1>
-    )
 
-}
-ReactDOM.render(<div>
-                    <navbar />
-                    <MainContent/>
-                </div>,
-                document.getElementById("root")
-                )
+const MainContent = () => (
+    <h1>My name is Pradeep I am learning react</h1>
+)
+
+
+ReactDOM.render((
+    <div>
+        <navbar />
+        <MainContent />
+    </div>
+), document.getElementById("app"))
